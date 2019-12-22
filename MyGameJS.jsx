d823ed54b73549ab689dc776b0d474da4e8df4f2
ainmax <aynurmahmutov@yandex.ru>
2019-12-22 11:53:19 +0300

    rename and version update

---
+++ MyGameJS.jsx
@@ -0,0 +1,51 @@
+<div class="myDiv">
+        <div class="StyleHover"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="41">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="42">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="43">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="44">
+        </div><br>
+        <div class="StyleHover"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="31">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="32">
+        </div>
+        <div class="StyleHover"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="33">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="34">
+        </div><br>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="21">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="22">
+        </div>
+        <div class="StyleHover"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="23">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="24">
+        </div><br>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: black !important; border: 4px solid black;"
+            id="11">
+        </div>
+        <div class="StyleHover"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="12">
+        </div>
+        <div class="StyleHoverNo" class="hyh"
+            style="display: inline-block; padding: 70px; background-color: green !important; border: 4px solid black;"
+            id="13">
+        </div>
+        <div class="StyleHoverNo"
+            style="display: inline-block; padding: 70px; background-color: white; border: 4px solid black;" id="14">
+        </div>
